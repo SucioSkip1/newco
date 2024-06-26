f693f85f99f7e83ef11c554747168dc58ce78ea3
SucioSkip1 <Shippudenlaloboy@hottmail.com>
2024-06-26 02:40:39 -0600

    Se ha agregado el filtro, pero sin eexito que se superpocisione

--- app/src/main/java/com/example/newco/MainActivity.java
+++ app/src/main/java/com/example/newco/MainActivity.java
@@ -25,4 +25,8 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(getApplicationContext(), crear_cuenta.class);
        startActivity(i);
     }
+    public void ir(View view){
+        Intent i = new Intent(getApplicationContext(),configurarPerfil.class);
+        startActivity(i);
+    }
 }
--- app/src/main/java/com/example/newco/configurarPerfil.java
+++ app/src/main/java/com/example/newco/configurarPerfil.java
@@ -1,17 +1,23 @@
 package com.example.newco;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class configurarPerfil extends AppCompatActivity {
-Button btnOpenDeveloperOptions,btnOpenColorSpaceSimulatorSettings;
+    private static final int REQUEST_OVERLAY_PERMISSION = 1;
+    private boolean isFilterApplied = false;
+    private Button btn_uno, btn_dos, btn_tres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +25,86 @@ Button btnOpenDeveloperOptions,btnOpenColorSpaceSimulatorSettings;
         setContentView(R.layout.activity_configurar_perfil);
 
 
-    }
-    public void openDeveloperOptions(View view) {
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-    
 
+
+        btn_uno = findViewById(R.id.btn_Deuteranopia);
+        btn_dos = findViewById(R.id.btn_Protanopia);
+        btn_tres = findViewById(R.id.btn_Tritanopia);
+        //Inicializar los botones
+        btn_uno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFilterApplied) {
+                    stopOverlayService();
+                } else {
+                    if (Settings.canDrawOverlays(configurarPerfil.this)) {
+                        startOverlayService();
+                    } else {
+                        requestOverlayPermission();
+                    }
+                }
+                isFilterApplied = !isFilterApplied;
+            }
+        });
+
+
+    }
+    protected void onResume() {
+        super.onResume();
+        checkOverlayPermission(); // Verificar el permiso de superposición al volver a la actividad principal
+    }
+    private void checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            // Si no se puede dibujar sobre otras aplicaciones, solicitar el permiso nuevamente
+            requestOverlayPermission();
+        } else {
+            // Si el permiso está concedido, iniciar el servicio de superposición
+            startOverlayService();
+        }
+    }
+    private void requestOverlayPermission() {
+       /* Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION);*/
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION);
+    }
+
+
+/*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_OVERLAY_PERMISSION) {
+            if (Settings.canDrawOverlays(this)) {
+                startOverlayService();
+            }
+        }
+    }
+    */
+protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_OVERLAY_PERMISSION) {
+
+            if (Settings.canDrawOverlays(this)) {
+                // Si el permiso se concedió, iniciar el servicio de superposición
+                startOverlayService();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+
+    }
+}
+    private void startOverlayService() {
+        Intent intent = new Intent(configurarPerfil.this, OverlayService.class);
+        startService(intent);
+        btn_uno.setText("Eliminar Filtro");
+    }
+
+    private void stopOverlayService() {
+        Intent intent = new Intent(configurarPerfil.this, OverlayService.class);
+        stopService(intent);
+        btn_uno.setText("Aplicar Filtro");
+    }
 
 }
